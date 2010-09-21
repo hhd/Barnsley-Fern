a6ff2522bccee10f3ccba5f47ce091d523f46bb8
Andrew Buntine <bunts@hhd.com.au>
2010-09-21 18:13:51 +1000

    Started laying out data structures

---
+++ fern.scm
@@ -0,0 +1,30 @@
+;;; Fractals
+;;; Barnsley's Fern
+;;; Andrew Buntine, 2010
+;;;
+;;; The Barnsley Fern is a fractal named after the British mathematician
+;;; Michael Barnsley. The fern is a basic example of a mathematically
+;;; generated pattern that can be reproducible at any magnification or reduction.
+;;;
+;;; Hopefully I can impress my Maths tutor with this.
+;;;
+;;; Usage:
+;;;   (aaaaaa)
+
+(require (lib "graphics.ss" "graphics"))
+
+; Colours to represent each function with.
+(define colours '("green" "green" "green" "green"))
+
+; Matrix values for each function.
+; Formula:
+;   f(x,y) = [a b][x] + [e]
+;            [c d][y]   [f]
+;
+; Format (where 'p' is the propability factor): 
+;   (a b c d e f p)
+(define matrix
+  #(#(0 0 0 0.16 0 0 0.01)
+    #(0.85 0.04 -0.04 0.85 0 1.6 0.85)
+    #(0.2 -0.26 0.23 0.22 0 1.6 0.07)
+    #(-0.15 0.28 0.26 0.24 0 0.44 0.07)))
